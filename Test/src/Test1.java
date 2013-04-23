6aa874d8486bc81a3b9e47e0cc338b4b71725990
liu <yh>
2013-04-23 20:58:26 +0800

    commit

--- Test/src/Test1.java
+++ Test/src/Test1.java
@@ -6,7 +6,7 @@ public class Test1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ÎÒµÄ²âÊÔ");
+		System.out.println("æˆ‘çš„æµ‹è¯•");
 	}
 
 }
